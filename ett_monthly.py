e5d33f1fa437b04124898ad7573938b86ecba1cf
souzatharsis <souza.tharsis@gmail.com>
2021-06-28 20:32:03 -0400

    add run on submit

--- ett_monthly.py
+++ ett_monthly.py
@@ -10,26 +10,37 @@ st.title('ETT Monthly Tournament Qualifier')
 #ids = [3831, 234850, 11002]
 
 collect_numbers = lambda x : [int(i) for i in re.split("[^0-9]", x) if i != ""]
-numbers = st.text_input("Please enter players IDs:")
-ids = (collect_numbers(numbers))
 
-start_date = st.text_input("START DATE (YYYY-MM-DD)")
-end_date  = st.text_input("END DATE (YYYY-MM-DD)")
+form = st.form(key='my-form')
+
+
+numbers = form.text_input("Please enter players IDs:")
+ids = (collect_numbers(numbers))
+start_date = form.text_input("START DATE (YYYY-MM-DD)")
+end_date  = form .text_input("END DATE (YYYY-MM-DD)")
+submit = form.form_submit_button('Submit')
 
 ELO_THRESHOLD = 2000
 
-players = [ett.Player(p_id) for p_id in ids]
 
-players_elo = [p.get_elo_history().rename(columns={"elo":p.name}) for p in
-               players if p.get_elo_history() is not None]
+if submit:
+    players = [ett.Player(p_id) for p_id in ids]
 
-group_elo_df = reduce(lambda df1,df2: pd.merge(df1,df2,how='outer', left_index=True, right_index=True),
-                      players_elo).ffill(axis = 0)
+    players_elo = [p.get_elo_history().rename(columns={"elo":(p.name+" ("+str(p.id)+")")}) for p in
+                   players if p.get_elo_history() is not None]
 
-monthly_stats = group_elo_df.loc[(group_elo_df.index >= start_date)  & (group_elo_df.index <= end_date)].describe().filter(like='m', axis=0).transpose()
-monthly_stats['qualified'] = monthly_stats['max'] >= ELO_THRESHOLD
-st.write(monthly_stats)
+    group_elo_df = reduce(lambda df1,df2: pd.merge(df1,df2,how='outer', left_index=True, right_index=True),
+                          players_elo).ffill(axis = 0)
 
-st.write("By highlanderNJ - Powered by pyETT")
+    monthly_stats = group_elo_df.loc[(group_elo_df.index >= start_date)  & (group_elo_df.index <= end_date)].describe().filter(like='m', axis=0).transpose()
+    monthly_stats['direct_entry'] = monthly_stats['max'] > ELO_THRESHOLD
+    monthly_stats['can_qualify'] = monthly_stats['min'] <= ELO_THRESHOLD
+    pd.options.display.float_format = '{:,.1f}'.format
+    monthly_stats
+
+
+'''
+By [highlanderNJ](https://www.elevenvr.net/eleven/348353) - Powered by [pyETT](https://github.com/souzatharsis/pyETT)
+'''
 
 
